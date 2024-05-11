f310ba3770cd12060bf0d379fb30ff8211ef2278
Ahmad Naeem Cheema <ad94ahmad@gmail.com>
2024-05-11 20:50:48 +0500

    Updated Project

--- capstone-project/vite.config.ts
+++ capstone-project/vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://leonardojacomussi.github.io/little-lemon"
+  base: "https://github.com/ahmadncheema/Meta-Front-End-Developer-Capstone-Project/capstone-project/"
 })
